3c531f6073a9fdf9f3878b3a8032b825b7640b54
HasithX <eranga.hasitha04@gmail.com>
2023-11-12 17:31:43 +0530

    add forms completed

--- main.js
+++ main.js
@@ -12,6 +12,13 @@ $(document).ready(function(){
     $("#table1").html(data);
     });
 
+    $.post("search.php",
+    {
+        teacher_name : ""
+    },
+    function(data, status){
+        $("#table2").html(data);
+    });
 
     $.post("search.php",
     {
@@ -21,9 +28,15 @@ $(document).ready(function(){
     $("#table3").html(data);
     });
 
-    
+    $.post("search.php",
+    {
+        get_teachers_list:""
+    },
+    function(data, status){
+    $("#tt_teachers").html(data);
+    });
 
-    //-----------search data here----------\
+    // //-----------search data here----------\
 
     //timetable
     $("#search1").keyup(function(){
@@ -65,8 +78,100 @@ $(document).ready(function(){
     });
 
     //---------add data here-------------\
+    // $("#add_timetable").click(function(){
+    //     $.post("search.php",
+    //     {
+    //         timetable_name:""
+    //     },
+    //     function(data, status){
+    //     $("#table1").html(data);
+    //     }),
+    //     $.post("search.php",
+    //     {
+    //         add_timetable : "",
+    //         tt_date : $("#tt_date").val(),
+    //         tt_period : $("#tt_period").val(),
+    //         tt_subject : $("#tt_subject").val(),
+    //         tt_class : $("#tt_class").val(),
+    //         tt_teachers : $("#tt_teachers").val()
+    //     },
+    //     function(data, status){
+    //         $("#f_p1").html(data);
+        
+    //     }),
+    //     $.post("search.php",
+    //     {
+    //         timetable_name:""
+    //     },
+    //     function(data, status){
+    //     $("#table1").html(data);
+    //     });
+        
+    // });
+
+
+    $("#add_teacher").click(function(){
+        $.post("search.php",
+        {
+            teacher_name : ""
+        },
+        function(data, status){
+            $("#table2").html(data);
+        }),
+        $.post("search.php",
+        {
+            add_teacher : "",
+            name : $("#T_name").val(),
+            id : $("#T_id").val(),
+            tp : $("#T_tp").val(),
+            subject : $("#T_subject").val(),
+            dob : $("#T_dob").val()
+        },
+        function(data, status){
+            $("#f_p2").html(data);
+        
+        }),
+        $.post("search.php",
+        {
+            teacher_name : ""
+        },
+        function(data, status){
+            $("#table2").html(data);
+        });
+    });
+
+    $("#add_student").click(function(){
+        $.post("search.php",
+        {
+            student_name:""
+        },
+        function(data, status){
+        $("#table3").html(data);
+        }),
+        $.post("search.php",
+        {
+            add_student : "",
+            S_name : $("#S_name").val(),
+            S_id : $("#S_id").val(),
+            S_tp : $("#S_tp").val(),
+            S_class : $("#S_class").val(),
+            S_dob : $("#S_dob").val(),
+            S_city : $("#S_city").val()
+        },
+        function(data, status){
+            $("#f_p3").html(data);
+        })
+        $.post("search.php",
+        {
+            student_name:""
+        },
+        function(data, status){
+        $("#table3").html(data);
+        });
+    });
     
 });
+
 
 
 function popup(point){
--- search.php
+++ search.php
@@ -22,57 +22,47 @@ function dbconection(){
 
 function checkPeriod() {
     $currentTime = date('H:i');
+    $currentDay = date('w'); // Get the current day of the week (0 for Sunday, 6 for Saturday)
+
+    // Return 'done' if it's Saturday or Sunday
+    if ($currentDay == 0 || $currentDay == 6) {
+        return 'done';
+    }
 
     $currentMinutes = (int)substr($currentTime, 0, 2) * 60 + (int)substr($currentTime, 3);
 
-    $startTime = 7 * 60; 
+    $startTime = 8 * 60; // School starts at 8:00 AM
+    $endTime = 13 * 60; // School ends at 1:00 PM
 
-    if ($currentMinutes >= $startTime) {
+    if ($currentMinutes >= $startTime && $currentMinutes < $endTime) {
         $elapsedMinutes = $currentMinutes - $startTime;
         $period = floor($elapsedMinutes / 45) + 1;
 
-        if ($period % 4 == 0 && $elapsedMinutes % 45 < 15) {
-            return 'break';
+        // Check if it's a break period
+        if ($period % 4 == 0 && $elapsedMinutes % 45 < 20) {
+            return 'brake';
         }
 
+        // Check if it's a school period
         if ($period <= 8) {
-            return $period;
+            return "period".$period;
         }
     }
+
+    // If it's not a school day or school hours, return 'done'
     return 'done';
 }
 
-function timetable_get_data(){
-    $result = $conn->query($sql);
-
-        if ($result->num_rows > 0){
-
-            while ($row = $result->fetch_assoc()) {
-
-               echo "<tr>";
-               echo "<td >".$row['time_table_id']."</td>";
-               echo "<td >".$row['date']."</td>";
-               echo "<td >".$row['class']."</td>";
-               echo "<td >".$row['subject']."</td>";
-               echo "<td >".$row['teacher']."</td>";
-               echo "<td >"."<i class='fa fa-edit'></i>"."</td>";
-               echo "<td >"."<i class='fa fa-trash' style='color:'red';'></i>"."</td>";
-               echo "</tr>";
-            }           
-
-        }  else{
-            echo "<tr>"."<td colspan = '5'>". "no data found". "</td>". "</tr>";
-            }; 
-}
 
 
-//-----------------------grt data----------------------\\
+//-----------------------get data----------------------\\
 function getTimetable(){
     
     $conn = dbconection();
 
     echo "<tr>";
     echo "<th>ID</th>";
+    echo "<th>Date</th>";
     echo "<th>Time</th>";
     echo "<th>Class</th>";
     echo "<th>Subject</th>";
@@ -99,18 +89,14 @@ function getTimetable(){
                 WHERE timetable.class LIKE '%$search%'
                 ORDER BY timetable.class
                 LIMIT 10;";
-
-            timetable_get_data()
-
-        }  
-        else {
-            $day = date('1');
+        }  else {
+            $day = date('w');
             $period = checkPeriod();
 
             if ($period == 'break'){
-                echo "<tr>"."<td colspan = '5'>". "..INTERVAL TIME..". "</td>". "</tr>";
+                echo "<tr>"."<td colspan = '8'>". "|------INTERVAL TIME-------|". "</td>". "</tr>";
             }elseif($period == 'done'){
-                echo "<tr>"."<td colspan = '5'>". "..AUTO TIMETABLE CLOSE..". "</td>". "</tr>";
+                echo "<tr>"."<td colspan = '8'>". "!---------AUTO TIMETABLE CLOSE---------!". "</td>". "</tr>";
             }else{
             $sql = "SELECT timetable.time_table_id,
                 timetable.date, 
@@ -126,15 +112,45 @@ function getTimetable(){
                timetable.time LIKE '%$period%'
                ORDER BY timetable.class
                 LIMIT 10;";
-
-            timetable_get_data()
             };
         }
-           
-    };
-    $conn-> close();
 
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0){
+                while ($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td >".$row['time_table_id']."</td>";
+                echo "<td >".$row['date']."</td>";
+                echo "<td >".$row['time']."</td>";
+                echo "<td >".$row['class']."</td>";
+                echo "<td >".$row['subject']."</td>";
+                echo "<td >".$row['teacher']."</td>";
+                echo "<td >"."<i class='fa fa-edit'></i>"."</td>";
+                echo "<td >"."<i class='fa fa-trash' style='color:red;'></i>"."</td>";
+                echo "</tr>";
+                }           
+            }  else{
+                echo "<tr>"."<td colspan = '8'>". "no data found". "</td>". "</tr>";
+                };        
+        };
+    $conn-> close();
 };
+
+function get_teachers_list(){
+
+    $conn = dbconection();
+    $sql = "SELECT T_ID, Name FROM teachers ORDER BY cast(T_ID as int) asc;";
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()){
+            echo "<option value='$row[T_ID]'>$row[Name]</option>";
+        }
+        
+    }
+    $conn-> close();
+}
 
 // teachers data
 
@@ -194,6 +210,7 @@ function getTeacheres(){
 $conn-> close();
 };
 
+//student data
 
 function getStudent(){
 
@@ -210,32 +227,48 @@ function getStudent(){
         echo "<th class='data_edit_colum'></th>";
         echo "</tr>";
 
-    if (isset($_POST["teacher_name"])) {
+    if (isset($_POST["student_name"])) {
 
-        $search = $_POST["teacher_name"];
+        $search = $_POST["student_name"];
 
         if ($search ==! ''){
 
-          $sql = "SELECT teachers.T_ID, teachers.Name, subject.name as 'subject', teachers.dob as 'DOB', teachers.Tele 
-            FROM teachers join subject on teachers.subject = subject.sub_id WHERE teachers.Name 
-            LIKE '%$search%' ORDER BY cast(teachers.T_ID as int) ASC;";
+          $sql = "SELECT 
+          students.S_ID,
+          students.name,
+          class.name as class,
+          students.City,
+          students.DOB,
+          students.Tele
+          FROM students
+          INNER JOIN class ON students.class = class.C_ID
+          WHERE students.name LIKE '%$search%'
+          ORDER BY cast(students.S_ID AS int) ASC;";
 
         }  else {
-            $sql = "SELECT  teachers.T_ID ,teachers.Name , subject.name AS 'subject' , teachers.dob as DOB , 
-            teachers.Tele FROM teachers INNER JOIN subject ON teachers.subject = subject.sub_id ORDER BY cast( teachers.T_ID as int) ASC;
-            ";
+            $sql = "SELECT 
+            students.S_ID,
+            students.name,
+            class.name as class,
+            students.City,
+            students.DOB,
+            students.Tele
+            FROM students
+            INNER JOIN class ON students.class = class.C_ID
+            ORDER BY cast(students.S_ID AS int) ASC;";
             };
 
-        $result = $conn->query($sql);
+        $result = $conn-> query($sql);
 
         if ($result->num_rows > 0){
 
             while ($row = $result->fetch_assoc()) {
-
+        
                echo "<tr>";
-               echo "<td >".$row['T_ID']."</td>";
-               echo "<td >".$row['Name']."</td>";
-               echo "<td >".$row['subject']."</td>";
+               echo "<td >".$row['S_ID']."</td>";
+               echo "<td >".$row['name']."</td>";
+               echo "<td >".$row['class']."</td>";
+               echo "<td >".$row['City']."</td>";
                echo "<td >".$row['DOB']."</td>";
                echo "<td >".$row['Tele']."</td>";
                echo "<td >"."<i class='fa fa-edit'></i>"."</td>";
@@ -245,11 +278,85 @@ function getStudent(){
             
 
         }  else{
-            echo "<tr>"."<td colspan = '5'>". "no data found". "</td>". "</tr>";
+            echo "<tr>"."<td colspan = '8'>". "no data found". "</td>". "</tr>";
             };
    };
 $conn-> close();
 };
+
+
+//------------------------------add data here------------------------\\
+
+function addtimetable(){
+
+    $conn = dbconection();
+
+    $tt_date = $_POST['tt_date'];
+    $tt_period = $_POST['tt_period'];
+    $tt_subject = $_POST['tt_subject'];
+    $tt_class = $_POST['tt_class'];
+    $tt_teachers = $_POST['tt_teachers'];
+    
+    $sql = "INSERT INTO `timetable`(`class`, `date`, `subject`, `teacher`, `time`) 
+    VALUES ('tt_class', 'tt_date', 'tt_subject', 'tt_teachers', 'tt_period');";
+    
+    $result = $conn -> query($sql);
+
+    if ($result ==! TRUE)  {
+        echo "Update failed: ". $conn -> error;
+    } 
+    
+    $conn -> close();
+
+};
+
+function addteacher(){
+
+    $conn = dbconection();
+
+    $name = $_POST['name'];
+    $id = $_POST['id'];
+    $tele = $_POST['tp'];
+    $subject = $_POST['subject'];
+    $dob = $_POST['dob'];
+    
+    $sql = "INSERT INTO teachers(`T_ID`, `Name`, `Tele`, `subject`, `dob`) 
+            VALUES ('$id','$name','$tele','$subject','$dob');";
+    
+    $result = $conn -> query($sql);
+
+    if ($result ==! TRUE)  {
+        echo "Update failed: ". $conn -> error;
+    } 
+    
+    $conn -> close();
+
+};
+
+
+function addstudent(){
+
+    $conn = dbconection();
+
+    $S_name = $_POST['S_name'];
+    $S_id = $_POST['S_id'];
+    $S_tp = $_POST['S_tp'];
+    $S_class = $_POST['S_class'];
+    $S_dob = $_POST['S_dob'];
+    $S_city = $_POST['S_city'];
+    
+    $sql = "INSERT INTO students(`S_ID`, `name`, `Tele`, `class`, `DOB`, `City`) 
+            VALUES ('$S_id','$S_name','$S_tp','$S_class','$S_dob','$S_city');";
+    
+    $result = $conn -> query($sql);
+
+    if ($result ==! TRUE)  {
+        echo "Update failed: ". $conn -> error;
+    } 
+    
+    $conn -> close();
+};
+
 
 
 
@@ -260,4 +367,27 @@ if (isset($_POST['timetable_name'])) {
 if (isset($_POST['teacher_name'])) {
     getTeacheres();
 }
+
+if (isset($_POST['student_name'])) {
+    getStudent();
+}
+
+if (isset($_POST['add_teacher'])) {
+    addteacher();
+}
+
+if (isset($_POST['add_student'])) {
+    addstudent();
+}
+
+if (isset($_POST['add_timetable'])) {
+    addtimetable();
+}
+
+if (isset($_POST['get_teachers_list'])) {
+    get_teachers_list();
+}
+
+
+
 ?>
